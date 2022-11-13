534a049c5ed8a5a3fe79d85bb7ab35bf3c396076
MWahyudinnn <118093356+MWahyudinnn@users.noreply.github.com>
2022-11-14 00:17:26 +0700

    remove dupe key

--- src/component/SliderSection.js
+++ src/component/SliderSection.js
@@ -17,7 +17,7 @@ const SliderSection = () => {
     // },
     dots: true,
     infinite: true,
-    speed: 1000,
+//     speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
